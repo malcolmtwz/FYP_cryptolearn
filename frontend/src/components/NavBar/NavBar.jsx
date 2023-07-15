8ef5552d550d8897c8f6dca452fbea56c224c471
malcolmtwz <malcolmtwz@gmail.com>
2023-07-15 19:37:53 +0800

    finally fixed navbar

---
+++ frontend/src/components/NavBar/NavBar.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './NavBar.css';
+
+function NavBar() {
+  return (
+    <nav className="navbar">
+      <h1>hi</h1>
+    </nav>
+  );
+}
+
+export default NavBar;
--- frontend/src/index.js
+++ frontend/src/index.js
@@ -2,10 +2,13 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
+import NavBar from './components/NavBar/NavBar';
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <App />
+    <NavBar />
   </React.StrictMode>
 );
