22e249ff95ab2fe74a523704c44b3ab05727fa92
malcolmtwz <malcolmtwz@gmail.com>
2023-07-24 00:46:09 +0800

    removed some cmments

--- frontend/src/screens/CaesarScreen/CaesarScreen.jsx
+++ frontend/src/screens/CaesarScreen/CaesarScreen.jsx
@@ -12,54 +12,6 @@ function CaesarScreen() {
         startEncryption();
       }, [originalMessage, shift]);
 
-    // originalInput.addEventListener("input", characterEntered, false);
-    // shiftInput.addEventListener("input", numberEntered, false);
-
-    // function characterEntered(e) {
-    //     originalMessage = e.target.value;
-    //     originalMessage = originalMessage.toLowerCase();
-    //     originalMessage = originalMessage.replace(/[^a-z]/, '');
-
-    //     e.target.value = originalMessage;
-
-    //     startEncryption();
-    // }
-
-    // function numberEntered(e) {
-    //     startEncryption();
-    // }
-  
-    // function startEncryption() {
-    //     let encryptedMessage = "";
-    //     let shift = shiftInput.value ? Number(shiftInput.value) : 0;
-    //     console.log(shift);
-
-    //     for (let letter of originalMessage) {
-    //         encryptedMessage += shiftLetter(letter, shift);
-    //     }
-        
-    //     console.log(encryptedMessage);
-    //     encryptedInput.value = encryptedMessage;
-    // }
-
-    // function shiftLetter(letter, shift) {
-    //     let newLetter = "";
-      
-    //     let letterCode = letter.charCodeAt(0);
-    //     let newLetterCode = letterCode + (shift % 26);
-      
-    //     if (newLetterCode < 97) {
-    //       newLetterCode += 26;
-    //     } else if (newLetterCode > 122) {
-    //       newLetterCode -= 26;
-    //     }
-      
-    //     newLetter = String.fromCharCode(newLetterCode);
-        
-    //     return newLetter;
-    
-    //   }
-
     function startEncryption() {
         let encryptedMessage = "";
         const shiftValue = shift ? Number(shift) : 0;
