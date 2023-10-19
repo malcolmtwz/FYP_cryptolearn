ba8be79e313a91a02b1496df2aff9715be789705
malcolmtwz <malcolmtwz@gmail.com>
2023-10-19 17:43:15 +0800

    Update NotesScreen.jsx

--- frontend/src/screens/HubScreens/NotesScreen/NotesScreen.jsx
+++ frontend/src/screens/HubScreens/NotesScreen/NotesScreen.jsx
@@ -10,8 +10,8 @@ function NotesScreen() {
         <div className="notes-screen">
             <div className="notes-background">
                 <h1 className="h-1">Welcome to the Encryption Tutorial</h1>
-                <Typography variant="body1" >This tutorial will teach you the basics of a few ciphers!</Typography>
-                <Typography variant="body1" > Please click on the sidebar on the left to proceed to specific cipher tutorials! You are encouraged to start from the top!</Typography>
+                <Typography variant="h5" >This tutorial will teach you the basics of a few ciphers!</Typography>
+                <Typography variant="h5" > Please click on the sidebar on the left to proceed to specific cipher tutorials! You are encouraged to start from the top!</Typography>
                 <ArrowLeftCircle  size={64} color="#61dafb" />
             </div>
         
